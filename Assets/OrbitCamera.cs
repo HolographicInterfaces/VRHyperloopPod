f6da31acaf8a54b7bf0a3b2bea53066c14ab5acc
MEGA\Michael <michael@byte56.com>
2017-02-01 20:40:56 -0800

    Actual initial commit of source. GoogleVR plugin with basic mockup of interface.

---
+++ Assets/OrbitCamera.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class OrbitCamera : MonoBehaviour {
+
+    public float offSet = 2;
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = (Camera.main.transform.forward * offSet) + Camera.main.transform.position;
+    }
+}
