bd9bdc3ea469709211d60e91df9e63125be22a1c
byte56 <michael@byte56.com>
2017-02-09 22:10:13 -0800

    Arc texture rendering added.

--- Assets/Scripts/UIScripts/CircularTextureGenerator.cs
+++ Assets/Scripts/UIScripts/CircularTextureGenerator.cs
@@ -1,37 +1,50 @@
 ﻿using UnityEngine;
-using System.Collections;
-using System;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Linq;
 
 public class CircularTextureGenerator : TextureGenerator {
 
     public Vector2 centerPositionOffset;
     public int radius;
     public int innerRadius;
+    
+
+    public int innerStartAngle;
+    public int innerEndAngle;
+
     public int startAngle;
     public int endAngle;
+
     public Color textureColor;
 
     public override void Generate()
     {
+        
+        targetTexture.SetPixels(Enumerable.Repeat(Color.clear, targetTexture.width * targetTexture.height).ToArray());
         int centerX = (int)(targetTexture.width / 2 + centerPositionOffset.x);
         int centerY = (int)(targetTexture.height / 2 + centerPositionOffset.y);
 
         float r, a, px, py;
         Color32[] tempArray = targetTexture.GetPixels32();
 
-        for (r = innerRadius; r <= radius; r+= .5f)
+        List<Vector2> points = new List<Vector2>();
+
+        for (a = innerStartAngle; a <= innerEndAngle; a ++)
         {
-            for (a = startAngle; a <= endAngle; a+= .5f)
-            {
-
-                px = centerX + r * Mathf.Sin(a * Mathf.Deg2Rad);
-                py = centerY + r * Mathf.Cos(a * Mathf.Deg2Rad);
-
-                targetTexture.SetPixel((int)px, (int)py, textureColor);
-                //tempArray[(int)(py * targetTexture.height + px)] = textureColor;
-            }
+            px = centerX + innerRadius * Mathf.Sin(a * Mathf.Deg2Rad);
+            py = centerY + innerRadius * Mathf.Cos(a * Mathf.Deg2Rad);
+            points.Add(new Vector2(px, py));
         }
-        //targetTexture.SetPixels32(tempArray);
+
+        for (a = endAngle; a >= startAngle; a--)
+        {
+            px = centerX + radius * Mathf.Sin(a * Mathf.Deg2Rad);
+            py = centerY + radius * Mathf.Cos(a * Mathf.Deg2Rad);
+            points.Add(new Vector2(px, py));
+        }
+
+        targetTexture.FillPolygon(points.ToArray(), textureColor);
         targetTexture.Apply();
     }
 }
--- Assets/Scripts/UIScripts/TextureGenerator.cs
+++ Assets/Scripts/UIScripts/TextureGenerator.cs
@@ -2,16 +2,21 @@
 using System.Collections;
 using UnityEngine.UI;
 
-
+[RequireComponent(typeof(RawImage))]
 public abstract class TextureGenerator : MonoBehaviour {
 
     public int textureWidth;
     public int textureHeight;
-
+    public bool useTargetImageSizeForTextureSize;
     protected Texture2D targetTexture;
 
     public virtual void Initialize()
     {
+        if (useTargetImageSizeForTextureSize)
+        {
+            textureWidth = (int)this.GetComponent<RawImage>().rectTransform.rect.size.x;
+            textureHeight = (int)this.GetComponent<RawImage>().rectTransform.rect.size.y;
+        }
         targetTexture = new Texture2D(textureWidth, textureHeight, TextureFormat.ARGB32, false, true);
     }
 
